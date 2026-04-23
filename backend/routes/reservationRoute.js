cf92257c8e0eb5dd180f852f96a5b1b73e3f44dc
Vishal Suryavanshi <Vishalmsk143@gmail.com>
2026-04-23 13:29:49 +0530

    Initial commit

---
+++ backend/routes/reservationRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import send_reservation from "../controller/reservation.js";
+
+const router = express.Router();
+
+router.post("/Sent", send_reservation);
+
+export default router;
